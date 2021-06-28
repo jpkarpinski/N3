3d0b53dc99b2304b844622d722e106d02c19de9b
jpkarpinski <80289680+jpkarpinski@users.noreply.github.com>
2021-06-28 16:38:29 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(
+    function(){
+    }
+);
+
+$('#bt_fnarguilas').click(
+    function(){
+        alert("F ! ! !");
+    }
+);
+
+$('#bt_gordo').click(
+    function(){
+        alert("Bryan mandou lembranças");
+    }
+);
+
+$('#bt_really').click(
+    function(){
+        alert("Speedy Beans me ajudaaaaaaa");
+    }
+);
+
+$('#bt_smoky').click(
+    function(){
+        alert("Smoky passou a fome kkkkkkk");
+    }
+);
+
+$('#bt_sobre').click(
+    function(){
+        $('#corpo').load("pages/sobre.html")
+    }
+)
